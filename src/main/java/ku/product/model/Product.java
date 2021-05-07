10533a5107828493f08678ab2d1c3e8786fb280f
Th <tharathorn.bu@ku.th>
2021-05-07 21:32:14 +0700

    Initial commit for Create an API service step

---
+++ src/main/java/ku/product/model/Product.java
@@ -0,0 +1,33 @@
+package ku.product.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+    private double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
